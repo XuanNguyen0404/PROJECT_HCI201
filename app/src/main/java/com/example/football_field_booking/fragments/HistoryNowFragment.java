59c0564161c75f6ad17815612e8577b6e3adcee1
Diep Dang Huy Hoang <61451281+HoangSE140693@users.noreply.github.com>
2021-10-12 08:15:57 +0700

    merge vs main

--- app/src/main/java/com/example/football_field_booking/fragments/HistoryNowFragment.java
+++ app/src/main/java/com/example/football_field_booking/fragments/HistoryNowFragment.java
@@ -12,51 +12,10 @@ import android.widget.TextView;
 
 import com.example.football_field_booking.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HistoryNowFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HistoryNowFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public HistoryNowFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HistoryNowFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HistoryNowFragment newInstance(String param1, String param2) {
-        HistoryNowFragment fragment = new HistoryNowFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
