60a7ed337e1459f45e551d874bd77f4ca6044c22
t72.89 <nvathy091@gmail.com>
2021-10-13 10:56:39 +0700

    done list promotion

--- app/src/main/java/com/example/football_field_booking/PromotionActivity.java
+++ app/src/main/java/com/example/football_field_booking/PromotionActivity.java
@@ -2,8 +2,10 @@ package com.example.football_field_booking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -13,8 +15,12 @@ public class PromotionActivity extends AppCompatActivity {
     private TextView textIgnore1;
     private TextView textIgnore2;
     private TextView textIgnore3;
+    private TextView textIgnore4;
+    private TextView textIgnore5;
+    private TextView textIgnore6;
     private MaterialToolbar topAppBar;
-
+    private TextView tvViewMore1;
+    private TextView tvViewMore2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +30,36 @@ public class PromotionActivity extends AppCompatActivity {
         textIgnore1 = findViewById(R.id.tvPriceIgnore1);
         textIgnore2 = findViewById(R.id.tvPriceIgnore2);
         textIgnore3 = findViewById(R.id.tvPriceIgnore3);
+        textIgnore4 = findViewById(R.id.tvPriceIgnore4);
+        textIgnore5 = findViewById(R.id.tvPriceIgnore5);
+        textIgnore6 = findViewById(R.id.tvPriceIgnore6);
+
+        tvViewMore1 = findViewById(R.id.tvViewMore1);
+        tvViewMore2 = findViewById(R.id.tvViewMore2);
+
 
         textIgnore1.setPaintFlags(textIgnore1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
         textIgnore2.setPaintFlags(textIgnore2.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
         textIgnore3.setPaintFlags(textIgnore3.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        textIgnore4.setPaintFlags(textIgnore4.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        textIgnore5.setPaintFlags(textIgnore5.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        textIgnore6.setPaintFlags(textIgnore6.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+        tvViewMore1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PromotionActivity.this, ListPromotionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        tvViewMore2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PromotionActivity.this, ListPromotionActivity.class);
+                startActivity(intent);
+            }
+        });
 
         topAppBar=findViewById(R.id.topAppBar);
         topAppBar.setTitle("Promotion");
